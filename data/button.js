f8956bfea73c9672ec3428726dd6d4732e9c667d
User630459 <mkmr2026@outlook.com>
2026-04-02 13:08:06 +0200

    init

---
+++ data/button.js
@@ -0,0 +1,14 @@
+window.lessonData = `
+PARA Dugme (Button) može da se pritisne
+PARA Dugme je:
+LIST
+ITEM Crveno
+ITEM Crno
+ITEM Sivo
+PARA Dugme sastoji se od:
+NUMBER
+ITEM Metala
+ITEM Dugmeta
+ITEM Bakra
+IMAGE button.jpg ignore
+`;
---
+++ data/main.js
@@ -0,0 +1,6 @@
+window.lessonData = `
+PARA Ovo je glavna stranica
+LINK https://sr.wikipedia.org/wiki/Hello_World Klikni me!
+PARA Lekcije:
+LINK #button Button
+`;
