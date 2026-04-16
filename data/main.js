f19415b5455c92078c53057084d1f3b87d3e8caa
Relja Stojanovic <mkmr2026@outlook.com>
2026-04-16 12:54:50 +0200

    Added the main page

--- data/main.js
+++ data/main.js
@@ -1,10 +1,31 @@
 window.lessonData = `
-PARA Ovo je glavna stranica
-LINK https://sr.wikipedia.org/wiki/Hello_World Klikni me!
-PARA Lekcije:
-PARA
+PARA Drobodošli!
+HEAD 3 1. UVOD
 LINK #intro 1.1 Uvod u mikrokontro
-PARA 
-LINK #button 2.1 Button
-PARA 
+LINK #intro_programing_syntax 1.2 Uvod u sintaksu programskog jezika
+EMPTY
+HEAD 3 2. LED
+LINK #LED 2.1 LED i otpornik
+LINK #LED_2 2.2 Kola sa dve LED-a
+LINK #LED_3 2.3 Kola sa tri LED-a
+EMPTY
+HEAD 3 3. BUTTON
+LINK #button 3.1 Button i LED
+LINK #potentiometer 3.2 Potenciometar i LED
+LINK #flip-flop 3.3 Flip-Flop
+LINK #shift_register 3.4 Shift Register
+EMPTY
+HEAD 3 4. DISPLAY
+LINK #7-segment_display_1-digit 4.1 7-segment (jedna ćelija)
+LINK #7-segment_display_1-digit_2 4.2 Dva 7-segment (jedna ćelija)
+LINK #7-segment_display_4-digit 4.3 7-segment (četiri ćelija)
+LINK #LED_matrix 4.4 LED matrix
+LINK #I2C 4.5 I2C
+LINK #LCD 4.6 LCD
+EMPTY
+HEAD 3 5. SENSOR
+LINK #ultrasonic_sensor 5.1 Ultrasonic
+LINK #DHT_11 5.2 DHT 11
+LINK #DHT_22 5.3 DHT 22
+LINK #HC-SR501 5.4 HC-SR501
 `;
